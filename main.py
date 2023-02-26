6053b82b847a07e8614390b58ef19126d4de7cbd
NitiC <68746060+nitic04@users.noreply.github.com>
2023-02-26 04:21:17 +0000

    added a password strength checker which returns the approximate strength of the generated password, also added some exception handling for if no options are selected

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import secrets
 import string
 
 def genPwd():
+  password = ''
   upperLetters = string.ascii_uppercase
   lowerLetters = string.ascii_lowercase
   digits = string.digits
@@ -16,15 +17,46 @@ def genPwd():
     choice = str(input('Include %s' % i + ' (Y/N)? ')).upper()
     if choice == 'Y':
       selection.append(optionsPrint.index(i))
-      
+
+  if len(selection) == 0:
+    return ''
   alphabet = ''
   for x in selection:
     alphabet += options[x]
 
-  password = ''
   for i in range(pwdLen):
     password += ''.join(secrets.choice(alphabet))
   print('Generated password:', password)
+  return password
+
+def check_pwd_strength(pwd):
+  '''
+  Criteria:
+  - at least 6 characters long, at most 20-characters long
+  - at least one lowercase letter, at least one uppercase letter, at least one symbol
+  - not have three repeating characters in a row
+  '''
+  points = 0
+  if 6 <= len(pwd) <= 20:
+    points += len(pwd) - 6
+
+  for i in pwd:
+    if i.isupper():
+      points += 1
+    elif i.islower():
+      points += 1
+    elif i in string.digits:
+      points += 1
+    else:
+      points += 1
+
+  if 0 <= points <= 2:
+    print('Password Strength: Weak')
+  elif 3 <= points <= 4:
+    print('Password Strength: Good')
+  else:
+    print('Password Strength: Strong')
 
 if __name__ == '__main__':
-  genPwd()
+  pwd = genPwd()
+  check_pwd_strength(pwd)
